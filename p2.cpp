47dc0862fb31e705177090d23ee5907335eee926
Nic <NicWebster96@gmail.com>
2019-03-10 18:06:15 -0700

    edit p2

--- p2.cpp
+++ p2.cpp
@@ -1,27 +1,5 @@
 // Created by Nicholas Webster
 
-/******************
-For this assignment, you will implement a simple multi-threaded program
-  to generate a log file.
-Your main function should create a file called <first initial + last name>.log.
-For example, my log file will be named myang.log. Then it will create
-  two separate threads, Thread-A and Thread-B.
-
-Both threads will open the log file and write to it.
-  Thread-A will write the numbers 1 to 26 ten times;
-    each time on a separate line.
-  Thread-B will write the letters A to Z ten times;
-    each time on a separate line. Use nested for loops.
-
-The main thread will write the following events to the log file:
-    program start
-    thread A created
-    thread B created
-    thread A terminated
-    thread B terminated
-    program end
-*******************/
-
 #include <iostream>
 #include <fstream>
 #include <thread>
